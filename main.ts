d9503315f581267e4977c7addfcd13b906f45c38
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 06:53:44 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+let seleccion = 0
+input.onGesture(Gesture.Shake, function () {
+    seleccion = randint(1, 3)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+    if (seleccion == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else if (seleccion == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+})
